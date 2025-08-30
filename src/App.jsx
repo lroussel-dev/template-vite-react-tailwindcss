c07e577b7ca2ca897b346e7ce94b6ee63741cb70
User <user@workstation.lan>
2025-08-30 07:58:09 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p>Ceci est un template vide avec Vite + React (router-dom) + TailwindCSS</p>
+    </>
+  )
+}
+
+export default App
