0ff75b8667accc28e78eefcaceffc3e7a1b7cd8d
LucasRoussel <dev@lucasroussel.com>
2025-12-19 07:14:36 +0100

    Added default routes (root and 404)

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <main>
+            <h1>404</h1>
+            <p>This page don't exist</p>
+        </main>
+    )
+}
+
+export default NotFound;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import NotFound from "./components/NotFound";
 import App from "./App";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 );
